1dee711a940b79a96cc66e0bbfb4c221bce695ff
Dinamiet <iceman.sm39@gmail.com>
2021-05-02 16:43:37 +0200

    Updated tests to fit within new structure

--- app/main_linklist.c
+++ app/main_linklist.c
@@ -1,185 +1,172 @@
-#include <stdio.h>
-
 #include "linkedlist.h"
+
+#include <stdio.h>
 
 #define NUM_NODES 10
 
+typedef struct
+{
+	Node	 List;
+	uint32_t Number;
+} DataNode;
+
 int main(int argc, char* argv[])
 {
-    LinkedList list;
-    LinkedList_Init(&list);
+	LinkedList list;
+	LinkedList_Init(&list);
 
-    Node nodes[NUM_NODES];
-    uint32_t data[NUM_NODES];
+	DataNode dataNodes[NUM_NODES];
 
-    // Setup nodes
-    for (size_t i = 0; i < NUM_NODES; i++)
-    {
-        data[i] = i;
-        nodes[i].Data= &data[i];
-    }
+	// Setup nodes
+	for (size_t i = 0; i < NUM_NODES; i++) { dataNodes[i].Number = i; }
 
-    // Add nodes to front
-    LinkedList_LinkHead(&list, &nodes[3]);
-    LinkedList_LinkHead(&list, &nodes[2]);
-    LinkedList_LinkHead(&list, &nodes[1]);
-    LinkedList_LinkHead(&list, &nodes[0]);
+	// Add nodes to front
+	LinkedList_LinkHead(&list, (Node*)&dataNodes[3]);
+	LinkedList_LinkHead(&list, (Node*)&dataNodes[2]);
+	LinkedList_LinkHead(&list, (Node*)&dataNodes[1]);
+	LinkedList_LinkHead(&list, (Node*)&dataNodes[0]);
 
-    LinkedList_LinkTail(&list, &nodes[4]);
-    LinkedList_LinkTail(&list, &nodes[5]);
-    LinkedList_LinkTail(&list, &nodes[6]);
-    LinkedList_LinkTail(&list, &nodes[7]);
+	LinkedList_LinkTail(&list, (Node*)&dataNodes[4]);
+	LinkedList_LinkTail(&list, (Node*)&dataNodes[5]);
+	LinkedList_LinkTail(&list, (Node*)&dataNodes[6]);
+	LinkedList_LinkTail(&list, (Node*)&dataNodes[7]);
 
-    Node* testNode;
-    uint32_t testData;
+	DataNode* testNode;
 
-    //Test 1;
-    testNode= LinkedList_UnlinkHead(&list);
-    if (!testNode)
-        return 1;
-    testData= *(uint32_t*)testNode->Data;
-    if (testData != 0)
-        return 1;
+	//Test 1;
+	testNode = (DataNode*)LinkedList_UnlinkHead(&list);
+	if (!testNode)
+		return 1;
+	if (testNode->Number != 0)
+		return 1;
 
-    // 2
-    testNode= LinkedList_UnlinkHead(&list);
-    if (!testNode)
-        return 2;
-    testData= *(uint32_t*)testNode->Data;
-    if (testData != 1)
-        return 2;
+	// 2
+	testNode = (DataNode*)LinkedList_UnlinkHead(&list);
+	if (!testNode)
+		return 2;
+	if (testNode->Number != 1)
+		return 2;
 
-    // 3
-    testNode= LinkedList_UnlinkTail(&list);
-    if (!testNode)
-        return 3;
-    testData= *(uint32_t*)testNode->Data;
-    if (testData != 7)
-        return 3;
+	// 3
+	testNode = (DataNode*)LinkedList_UnlinkTail(&list);
+	if (!testNode)
+		return 3;
+	if (testNode->Number != 7)
+		return 3;
 
-    // 4
-    testNode= LinkedList_UnlinkTail(&list);
-    testNode= LinkedList_UnlinkTail(&list);
-    testNode= LinkedList_UnlinkTail(&list);
-    testNode= LinkedList_UnlinkTail(&list);
-    testNode= LinkedList_UnlinkTail(&list);
-    if (!testNode)
-        return 4;
-    testData= *(uint32_t*)testNode->Data;
-    if (testData != 2)
-        return 4;
+	// 4
+	testNode = (DataNode*)LinkedList_UnlinkTail(&list);
+	testNode = (DataNode*)LinkedList_UnlinkTail(&list);
+	testNode = (DataNode*)LinkedList_UnlinkTail(&list);
+	testNode = (DataNode*)LinkedList_UnlinkTail(&list);
+	testNode = (DataNode*)LinkedList_UnlinkTail(&list);
+	if (!testNode)
+		return 4;
+	if (testNode->Number != 2)
+		return 4;
 
-    // 5
-    testNode= LinkedList_UnlinkTail(&list);
-    if (testNode != NULL)
-        return 5;
+	// 5
+	testNode = (DataNode*)LinkedList_UnlinkTail(&list);
+	if (testNode != NULL)
+		return 5;
 
-    testNode= LinkedList_UnlinkHead(&list);
-    if (testNode != NULL)
-        return 5;
+	testNode = (DataNode*)LinkedList_UnlinkHead(&list);
+	if (testNode != NULL)
+		return 5;
 
-    // 6
-    LinkedList_LinkTail(&list, &nodes[9]);
-    testNode= LinkedList_UnlinkHead(&list);
-    if (!testNode)
-        return 6;
-    testData= *(uint32_t*)testNode->Data;
-    if (testData != 9)
-        return 6;
+	// 6
+	LinkedList_LinkTail(&list, (Node*)&dataNodes[9]);
+	testNode = (DataNode*)LinkedList_UnlinkHead(&list);
+	if (!testNode)
+		return 6;
+	if (testNode->Number != 9)
+		return 6;
 
-    LinkedList_LinkHead(&list, &nodes[8]);
-    testNode= LinkedList_UnlinkHead(&list);
-    if (!testNode)
-        return 6;
-    testData= *(uint32_t*)testNode->Data;
-    if (testData != 8)
-        return 6;
+	LinkedList_LinkHead(&list, (Node*)&dataNodes[8]);
+	testNode = (DataNode*)LinkedList_UnlinkHead(&list);
+	if (!testNode)
+		return 6;
+	if (testNode->Number != 8)
+		return 6;
 
-    LinkedList_LinkHead(&list, &nodes[2]);
-    testNode= LinkedList_UnlinkTail(&list);
-    if (!testNode)
-        return 6;
-    testData= *(uint32_t*)testNode->Data;
-    if (testData != 2)
-        return 6;
+	LinkedList_LinkHead(&list, (Node*)&dataNodes[2]);
+	testNode = (DataNode*)LinkedList_UnlinkTail(&list);
+	if (!testNode)
+		return 6;
+	if (testNode->Number != 2)
+		return 6;
 
-    LinkedList_LinkTail(&list, &nodes[0]);
-    testNode= LinkedList_UnlinkTail(&list);
-    if (!testNode)
-        return 6;
-    testData= *(uint32_t*)testNode->Data;
-    if (testData != 0)
-        return 6;
+	LinkedList_LinkTail(&list, (Node*)&dataNodes[0]);
+	testNode = (DataNode*)LinkedList_UnlinkTail(&list);
+	if (!testNode)
+		return 6;
+	if (testNode->Number != 0)
+		return 6;
 
-    // Checking itteration
-    // populate list
-    LinkedList_LinkHead(&list, &nodes[0]);
-    LinkedList_LinkTail(&list, &nodes[2]);
-    LinkedList_LinkTail(&list, &nodes[4]);
-    LinkedList_LinkTail(&list, &nodes[6]);
-    LinkedList_LinkTail(&list, &nodes[8]);
+	// Checking itteration
+	// populate list
+	LinkedList_LinkHead(&list, (Node*)&dataNodes[0]);
+	LinkedList_LinkTail(&list, (Node*)&dataNodes[2]);
+	LinkedList_LinkTail(&list, (Node*)&dataNodes[4]);
+	LinkedList_LinkTail(&list, (Node*)&dataNodes[6]);
+	LinkedList_LinkTail(&list, (Node*)&dataNodes[8]);
 
-    // Checking forward links
-    testNode= list.Head;
-    for (size_t i = 0; i < NUM_NODES; i+= 2)
-    {
-        if (testNode == NULL)
-            return 7;
-        testData= *(uint32_t*)testNode->Data;
-        if (testData != i)
-            return 7;
+	// Checking forward links
+	testNode = (DataNode*)list.Head;
+	for (size_t i = 0; i < NUM_NODES; i += 2)
+	{
+		if (testNode == NULL)
+			return 7;
+		if (testNode->Number != i)
+			return 7;
 
-        testNode= testNode->Next;
-    }
+		testNode = (DataNode*)testNode->List.Next;
+	}
 
-    if (testNode != list.Head)
-        return 7;
+	if (testNode != (DataNode*)list.Head)
+		return 7;
 
-    // Checking backward links
-    testNode= list.Tail;
-    for (int i = NUM_NODES-2; i >= 0; i-= 2)
-    {
-        if (testNode == NULL)
-            return 8;
-        testData= *(uint32_t*)testNode->Data;
-        if (testData != i)
-            return 8;
+	// Checking backward links
+	testNode = (DataNode*)list.Tail;
+	for (int i = NUM_NODES - 2; i >= 0; i -= 2)
+	{
+		if (testNode == NULL)
+			return 8;
+		if (testNode->Number != i)
+			return 8;
 
-        testNode= testNode->Prev;
-    }
+		testNode = (DataNode*)testNode->List.Prev;
+	}
 
-    if (testNode != list.Tail)
-        return 8;
+	if (testNode != (DataNode*)list.Tail)
+		return 8;
 
+	testNode = (DataNode*)LinkedList_UnlinkHead(&list);
+	LinkedList_LinkTail(&list, (Node*)testNode);
 
-    testNode= LinkedList_UnlinkHead(&list);
-    LinkedList_LinkTail(&list, testNode);
+	// Checking forward links
+	testNode = (DataNode*)list.Head->Prev;
+	for (size_t i = 0; i < NUM_NODES; i += 2)
+	{
+		if (testNode == NULL)
+			return 9;
+		if (testNode->Number != i)
+			return 9;
 
-    // Checking forward links
-    testNode= list.Head->Prev;
-    for (size_t i = 0; i < NUM_NODES; i+= 2)
-    {
-        if (testNode == NULL)
-            return 9;
-        testData= *(uint32_t*)testNode->Data;
-        if (testData != i)
-            return 9;
+		testNode = (DataNode*)testNode->List.Next;
+	}
 
-        testNode= testNode->Next;
-    }
+	// Checking backward links
+	testNode = (DataNode*)list.Tail->Prev;
+	for (int i = NUM_NODES - 2; i >= 0; i -= 2)
+	{
+		if (testNode == NULL)
+			return 10;
+		if (testNode->Number != i)
+			return 10;
 
-    // Checking backward links
-    testNode= list.Tail->Prev;
-    for (int i = NUM_NODES-2; i >= 0; i-= 2)
-    {
-        if (testNode == NULL)
-            return 10;
-        testData= *(uint32_t*)testNode->Data;
-        if (testData != i)
-            return 10;
+		testNode = (DataNode*)testNode->List.Prev;
+	}
 
-        testNode= testNode->Prev;
-    }
-
-    return 0;
+	return 0;
 }
