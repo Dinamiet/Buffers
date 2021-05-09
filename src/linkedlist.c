eaf2e291f5b0dcde0939f3368f6cd0e9960058b1
Dinamiet <iceman.sm39@gmail.com>
2021-05-09 14:02:38 +0200

    Added comments

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -31,10 +31,12 @@ Node* LinkedList_LinkAfter(LinkedList* list, Node* afterNode, Node* node)
 		return NULL;
 	}
 
+	// Update links
 	node->Prev		= afterNode;
 	node->Next		= afterNode->Next;
 	afterNode->Next = node;
 
+	// If node is added after tail, references needs to be updated
 	if (list->Tail == afterNode)
 	{
 		list->Tail = list->Head->Prev = node;
@@ -50,10 +52,12 @@ Node* LinkedList_LinkBefore(LinkedList* list, Node* beforeNode, Node* node)
 		return NULL;
 	}
 
+	// Update links
 	node->Next		 = beforeNode;
 	node->Prev		 = beforeNode->Prev;
 	beforeNode->Prev = node;
 
+	// If node is added before head, references needs to be updated
 	if (list->Head == beforeNode)
 	{
 		list->Head = list->Tail->Next = node;
@@ -66,26 +70,31 @@ Node* LinkedList_UnlinkNode(LinkedList* list, Node* node)
 {
 	if (node == NULL)
 		return NULL;
+
+	// Checks for last link in list
 	if (list->Head == node && list->Tail == node)
 	{
-		list->Head = list->Tail = NULL;
-		node->Next = node->Prev = NULL;
+		list->Head = list->Tail = node->Next = node->Prev = NULL;
 		return node;
 	}
 
+	// Check if head is being removed
 	if (node == list->Head)
 	{
 		list->Head = node->Next;
 	}
 
+	// Check if tail is being removed
 	if (node == list->Tail)
 	{
 		list->Tail = node->Prev;
 	}
 
+	// Remove node from list
 	node->Next->Prev = node->Prev;
 	node->Prev->Next = node->Next;
 
+	// Cleanup node
 	node->Next = node->Prev = NULL;
 	return node;
 }
