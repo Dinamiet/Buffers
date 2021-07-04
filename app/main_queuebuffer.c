20b7299e5b5095aae3c134161d778b5fc996f1db
Dinamiet <iceman.sm39@gmail.com>
2021-07-04 19:19:40 +0200

    Code formatting

--- app/main_queuebuffer.c
+++ app/main_queuebuffer.c
@@ -2,25 +2,25 @@
 
 #include <stdio.h>
 
-#define NUM_NODES 3
+#define NUM_NODES		 3
 #define TEST_BUFFER_SIZE 5
 
 int main()
 {
 	QueueBuffer queue;
-	uint32_t buffer[NUM_NODES]= { 0 };
-	uint32_t item;
+	uint32_t	buffer[NUM_NODES] = {0};
+	uint32_t	item;
 
 	QueueBuffer_Init(&queue, (void*)buffer, sizeof(buffer[0]), NUM_NODES);
 
 	if (QueueBuffer_Remove(&queue, (void*)&item))
 		return 1;
 
-	item= 10;
+	item = 10;
 	if (!QueueBuffer_Add(&queue, (void*)&item))
 		return 2;
 
-	item= 20;
+	item = 20;
 	if (!QueueBuffer_Add(&queue, (void*)&item))
 		return 3;
 
@@ -36,21 +36,21 @@ int main()
 
 	for (size_t i = 0; i < NUM_NODES; i++)
 	{
-		item= 100*i;
+		item = 100 * i;
 		if (!QueueBuffer_Add(&queue, (void*)&item))
-			return 100+i;
+			return 100 + i;
 	}
 
-	item= 20;
+	item = 20;
 	if (QueueBuffer_Add(&queue, (void*)&item))
 		return 3;
 
 	for (size_t i = 0; i < NUM_NODES; i++)
 	{
 		if (!QueueBuffer_Remove(&queue, (void*)&item))
-			return 200+i;
-		if (item != i*100)
-			return 11+i;
+			return 200 + i;
+		if (item != i * 100)
+			return 11 + i;
 	}
 
 	if (QueueBuffer_Remove(&queue, (void*)&item))
--- app/main_stackbuffer.c
+++ app/main_stackbuffer.c
@@ -2,25 +2,25 @@
 
 #include <stdio.h>
 
-#define NUM_NODES 3
+#define NUM_NODES		 3
 #define TEST_BUFFER_SIZE 5
 
 int main()
 {
 	StackBuffer stack;
-	uint32_t buffer[NUM_NODES]= { 0 };
-	uint32_t item;
+	uint32_t	buffer[NUM_NODES] = {0};
+	uint32_t	item;
 
 	StackBuffer_Init(&stack, (void*)buffer, sizeof(buffer[0]), NUM_NODES);
 
 	if (StackBuffer_Remove(&stack, (void*)&item))
 		return 1;
 
-	item= 10;
+	item = 10;
 	if (!StackBuffer_Add(&stack, (void*)&item))
 		return 2;
 
-	item= 20;
+	item = 20;
 	if (!StackBuffer_Add(&stack, (void*)&item))
 		return 3;
 
@@ -36,21 +36,21 @@ int main()
 
 	for (size_t i = 0; i < NUM_NODES; i++)
 	{
-		item= 100*i;
+		item = 100 * i;
 		if (!StackBuffer_Add(&stack, (void*)&item))
-			return 100+i;
+			return 100 + i;
 	}
 
-	item= 20;
+	item = 20;
 	if (StackBuffer_Add(&stack, (void*)&item))
 		return 3;
 
 	for (size_t i = 0; i < NUM_NODES; i++)
 	{
 		if (!StackBuffer_Remove(&stack, (void*)&item))
-			return 200+i;
-		if (item != ((NUM_NODES-1)*100) - i*100)
-			return 11+i;
+			return 200 + i;
+		if (item != ((NUM_NODES - 1) * 100) - i * 100)
+			return 11 + i;
 	}
 
 	if (StackBuffer_Remove(&stack, (void*)&item))
