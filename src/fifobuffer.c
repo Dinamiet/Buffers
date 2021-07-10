3c2294d9ed4ea0252fdeb06bc90905c81e0149ac
Dinamiet <iceman.sm39@gmail.com>
2021-07-10 16:58:59 +0200

    Head and tail increment moved

--- src/fifobuffer.c
+++ src/fifobuffer.c
@@ -18,9 +18,9 @@ void* FifoBuffer_Add(FifoBuffer* fifo)
 	if (fifo->Full)
 		return NULL;
 
-	uint32_t offset = fifo->Head * fifo->ElementSize;
+	uint32_t offset = fifo->Head++ * fifo->ElementSize;
 
-	if (++fifo->Head >= fifo->NumElements) // Wrap around at buffer end
+	if (fifo->Head >= fifo->NumElements) // Wrap around at buffer end
 		fifo->Head = 0;
 
 	fifo->Full	= fifo->Head == fifo->Tail;
@@ -34,9 +34,9 @@ void* FifoBuffer_Remove(FifoBuffer* fifo)
 	if (fifo->Empty)
 		return NULL;
 
-	uint32_t offset = fifo->Tail * fifo->ElementSize;
+	uint32_t offset = fifo->Tail++ * fifo->ElementSize;
 
-	if (++fifo->Tail >= fifo->NumElements) // Wrap around at buffer end
+	if (fifo->Tail >= fifo->NumElements) // Wrap around at buffer end
 		fifo->Tail = 0;
 
 	fifo->Empty = fifo->Head == fifo->Tail;
