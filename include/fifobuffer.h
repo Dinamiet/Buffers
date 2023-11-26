c1844c063e52077603f0078655a6a03d4b4ad804
Dinamiet <iceman.sm39@gmail.com>
2023-11-26 13:07:45 +0200

    Reworked Fifo buffer

--- include/fifobuffer.h
+++ include/fifobuffer.h
@@ -3,20 +3,25 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 typedef struct
 {
-	void*	 Buffer;
-	size_t ElementSize;
-	size_t NumElements;
-	size_t Head;
-	size_t Tail;
-	bool	 Full;
-	bool	 Empty;
+	uint8_t* Buffer;
+	size_t   ElementSize;
+	size_t   NumElements;
+	size_t   AddIndex;
+	size_t   RemoveIndex;
+	bool     LastAdd;
 } FifoBuffer;
 
-void  FifoBuffer_Init(FifoBuffer* fifo, void* buff, size_t elementSize, size_t numElements);
-void* FifoBuffer_Add(FifoBuffer* fifo);
-void* FifoBuffer_Remove(FifoBuffer* fifo);
+void   FifoBuffer_Init(FifoBuffer* fifo, void* buff, size_t elementSize, size_t numElements);
+bool   FifoBuffer_Full(FifoBuffer* fifo);
+bool   FifoBuffer_Empty(FifoBuffer* fifo);
+size_t FifoBuffer_Used(FifoBuffer* fifo);
+size_t FifoBuffer_Free(FifoBuffer* fifo);
+void*  FifoBuffer_Add(FifoBuffer* fifo);
+void*  FifoBuffer_Remove(FifoBuffer* fifo);
+void   FifoBuffer_Clear(FifoBuffer* fifo);
 
 #endif
